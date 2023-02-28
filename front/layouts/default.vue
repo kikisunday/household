f8b39918a68487b389328b43770ebe93b374d3c7
kikisunday <kikisunday.110404@gmail.com>
2023-02-28 20:49:59 +0900

    :sparkles: ヘッダーフッター

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,8 +1,32 @@
-<script setup></script>
+<script setup>
+const links = [''];
+</script>
 
 <template>
-  <div>
-    <nav>twitter動画保存庫</nav>
+  <v-app>
+    <v-app-bar color="teal-darken-4">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>ドッキリ動画保存庫</v-app-bar-title>
+    </v-app-bar>
     <slot />
-  </div>
+    <v-footer class="bg-grey-lighten-1 fotter">
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          variant="text"
+          class="mx-2"
+          rounded="xl"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="text-center mt-4" cols="12">
+          {{ new Date().getFullYear() }} — <strong>ドッキリ動画保存庫</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
